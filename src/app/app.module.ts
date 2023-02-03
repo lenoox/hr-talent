4743a3d7759a0c5b0aa60468246a8668cba07a2f
lenoox <62141973+lenoox@users.noreply.github.com>
2023-02-03 12:06:57 +0100

    add job offer list - add material package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ModulesModule} from "./modules/modules.module";
 import {CoreModule} from "./core/core.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import {CoreModule} from "./core/core.module";
     BrowserModule,
     AppRoutingModule,
     ModulesModule,
-    CoreModule
+    CoreModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
